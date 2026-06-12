6d30383bfa1715e8d84bda36481c8c5b5d8566b0
GB <idk>
2026-06-11 20:11:17 -0400

    feat:password hashing

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -27,7 +27,7 @@ public class User {
     @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "password", nullable = false,length = 255)
     private String password;
 
     @ManyToOne
